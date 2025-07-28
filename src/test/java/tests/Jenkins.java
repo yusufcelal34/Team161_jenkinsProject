a7e210e88c6ce1cca183d343c7f1169d77bedae6
Yusuf Celal <yusufcelal34@gmail.com>
2025-07-28 16:24:54 +0300

    first commit

---
+++ src/test/java/tests/Jenkins.java
@@ -0,0 +1,85 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class Jenkins {
+
+
+    @Test
+    void Test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test3() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test4() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test5() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test6() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test7() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test8() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test9() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test10() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test11() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test12() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test13() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test14() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test15() {
+        assertTrue(true);
+    }
+}
+
